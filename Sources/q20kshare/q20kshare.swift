6145d5e2ad1d9498482694966a2d29e5c02ea2a7
Bill Donner <billdonner@gmail.com>
2023-06-06 19:34:23 -0500

    modified

--- Sources/q20kshare/q20kshare.swift
+++ Sources/q20kshare/q20kshare.swift
@@ -1,14 +1,29 @@
 import Foundation
 public struct q20kshare {
     public private(set) var text = "Q20KSHARE"
-   public private(set) var version = "0.1"
+    public private(set) var version = "0.2"
     public init() {
     }
 }
 
 /* Challenge(s) is the basic heart of q20k world */
+
+public struct AIReturns: Codable {
+  public let question: String
+  public let topic: String
+  public let hint:String // a hint to show if the user needs help
+  public let answers: [String]
+  public let correct: String // which answer is correct
+  public let explanation: String? // reasoning behind the correctAnswer
+  public let article: String?// URL of article about the correct Answer
+  public let image:String? // URL of image of correct Answer
+
+   public func toChallenge() -> Challenge {
+      Challenge(question: self.question, topic: self.topic, hint:self.hint, answers:self.answers, correct: self.correct,id:UUID().uuidString,date:Date())
+  }
+}
 public struct Challenge : Codable,Equatable,Hashable  {
-  public init(question: String, topic: String, hint: String, answers: [String], correct: String, explanation: String? = nil, article: String? = nil, image: String? = nil) {
+  internal init(question: String, topic: String, hint: String, answers: [String], correct: String, explanation: String? = nil, article: String? = nil, image: String? = nil, id: String? = nil, date: Date? = nil) {
     self.question = question
     self.topic = topic
     self.hint = hint
@@ -17,7 +32,11 @@ public struct Challenge : Codable,Equatable,Hashable  {
     self.explanation = explanation
     self.article = article
     self.image = image
+    self.id = id
+    self.date = date
   }
+  
+
   
   public let question: String
   public let topic: String
@@ -27,6 +46,10 @@ public struct Challenge : Codable,Equatable,Hashable  {
   public let explanation: String? // reasoning behind the correctAnswer
   public let article: String?// URL of article about the correct Answer
   public let image:String? // URL of image of correct Answer
+  // these fields are hidden from the ai and filled in by prepper
+  public let id:String? // can be real uuid
+  public let date:Date? // hmmm
+  
   
   public static func decodeArrayFrom(data:Data) throws -> [Challenge]{
     try JSONDecoder().decode([Challenge].self, from: data)
