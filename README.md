0c593aecfdc8b20b8bd5ee137a84dcaba4bd08c2
bill donner <billdonner@gmail.com>
2023-10-09 13:21:51 -0500

    0.4.1

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # q20kshare
 
-Freeport.Software 0.3.5
-again
+Freeport.Software 0.4.1
+
 
 shared data structures and code for q20k family
 
--- Sources/q20kshare/q20kshare.swift
+++ Sources/q20kshare/q20kshare.swift
@@ -1,7 +1,7 @@
 import Foundation
 public struct q20kshare {
   public private(set) var text = "Q20KSHARE"
-  public private(set) var version = "0.3.5"
+  public private(set) var version = "0.4.1"
   public init() {
   }
 }
