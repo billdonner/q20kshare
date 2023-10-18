05266048197b2f58eeea45ceb98f92b5ab9de108
bill donner <billdonner@gmail.com>
2023-10-18 12:10:36 -0500

    0.5.01

--- multibuild.zsh
+++ multibuild.zsh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 echo "******-------> build snarkyc"
-./q20kshare/bild.zsh snarkyc
+/Users/billdonner/Documents/q20kshare/bild.zsh snarkyc
 echo "******-------> build pumperc"
-./q20kshare/bild.zsh pumperc
+/Users/billdonner/Documents/q20kshare/bild.zsh pumperc
 echo "******-------> build prepperc"
-./q20kshare/bild.zsh prepperc
+/Users/billdonner/Documents/q20kshare/bild.zsh prepperc
 echo "******-------> build liedetectorc"
-./q20kshare/bild.zsh liedetectorc
+/Users/billdonner/Documents/q20kshare/bild.zsh liedetectorc
 echo "******-------> build blenderc"
-./q20kshare/bild.zsh blenderc
+/Users/billdonner/Documents/q20kshare/bild.zsh blenderc
---
+++ multiclone.zsh
@@ -0,0 +1,5 @@
+git clone https://github.com/billdonner/snarkyc
+git clone https://github.com/billdonner/pumperc
+git clone https://github.com/billdonner/prepperc
+git clone https://github.com/billdonner/liedetectorc
+git clone https://github.com/billdonner/blenderc 
