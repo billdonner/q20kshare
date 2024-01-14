9b8088a3e80e8e954fb62aaf50307b5b9f2a23c5
bill donner <billdonner@gmail.com>
2024-01-14 11:52:11 -0600

    0.5.22

--- Sources/q20kshare/q20kshare.swift
+++ Sources/q20kshare/q20kshare.swift
@@ -1,7 +1,7 @@
 import Foundation
 public struct q20kshare {
   public private(set) var text = "Q20KSHARE"
-  public private(set) var version = "0.5.21"
+  public private(set) var version = "0.5.22"
   public init() {
   }
 }
@@ -27,7 +27,7 @@ public struct TopicGroup : Codable {
 }
 
 public struct Topic : Codable {
-  public init(name: String, subject: String,  pic: String, notes: String, subtopics:[String]? = nil) {
+  public init(name: String, subject: String,  pic: String, notes: String, subtopics:[String]) {
     self.name = name
     self.subject = subject
     self.pic = pic
